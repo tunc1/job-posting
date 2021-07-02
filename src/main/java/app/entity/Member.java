bd9d73ae276c41c582421dd876c3265fcd9f91db
user <user>
2021-07-02 21:12:45 +0300

    Update Member implement UserDetails

--- src/main/java/app/entity/Member.java
+++ src/main/java/app/entity/Member.java
@@ -1,19 +1,78 @@
 package app.entity;
 
+import java.util.Collection;
+import java.util.List;
+
 import javax.persistence.*;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Entity
-public class Member
+public class Member implements UserDetails
 {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-    private String name,lastName;
+    private String name,lastName,username,title;
     @Column(unique=true,nullable=false)
     private String email;
+    @JsonProperty(access=JsonProperty.Access.WRITE_ONLY)
+    String password;
     @ManyToOne
     private Company company;
-    private String title;
+    private boolean accountNonExpired,accountNonLocked,credentialsNonExpired,enabled;
+    public boolean isAccountNonExpired()
+    {
+        return accountNonExpired;
+    }
+    public void setAccountNonExpired(boolean accountNonExpired)
+    {
+        this.accountNonExpired = accountNonExpired;
+    }
+    public boolean isAccountNonLocked()
+    {
+        return accountNonLocked;
+    }
+    public void setAccountNonLocked(boolean accountNonLocked)
+    {
+        this.accountNonLocked = accountNonLocked;
+    }
+    public boolean isCredentialsNonExpired()
+    {
+        return credentialsNonExpired;
+    }
+    public void setCredentialsNonExpired(boolean credentialsNonExpired)
+    {
+        this.credentialsNonExpired = credentialsNonExpired;
+    }
+    public boolean isEnabled()
+    {
+        return enabled;
+    }
+    public void setEnabled(boolean enabled)
+    {
+        this.enabled = enabled;
+    }
+    public void setUsername(String username)
+    {
+        this.username=username;
+    }
+    public void setPassword(String password)
+    {
+        this.password=password;
+    }
+    public String getUsername()
+    {
+        return username;
+    }
+    public String getPassword()
+    {
+        return password;
+    }
     public String getEmail()
     {
         return email;
@@ -61,5 +120,9 @@ public class Member
     public void setTitle(String title)
     {
         this.title=title;
+    }
+    public Collection<? extends GrantedAuthority> getAuthorities()
+    {
+        return List.of(new SimpleGrantedAuthority("ROLE_Member"));
     }
 }
