e40fdc4499dd605bdd7029239cdec1ae94a33062
user <user>
2021-07-03 12:10:58 +0300

    Add Exception Handler

---
+++ src/main/java/app/exception/ExceptionMessage.java
@@ -0,0 +1,18 @@
+package app.exception;
+
+public class ExceptionMessage
+{
+    private String message;
+    public ExceptionMessage(String message)
+    {
+        this.message=message;
+    }
+    public String getMessage()
+    {
+        return message;
+    }
+    public void setMessage(String message)
+    {
+        this.message=message;
+    }
+}
---
+++ src/main/java/app/exception/RestControllerAdvice.java
@@ -0,0 +1,24 @@
+package app.exception;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@org.springframework.web.bind.annotation.RestControllerAdvice
+public class RestControllerAdvice
+{
+    @ExceptionHandler(value={EntityNotFoundException.class})
+    @ResponseStatus(value=HttpStatus.NOT_FOUND)
+    public ExceptionMessage entityNotFoundExceptionHandler(Exception e)
+    {
+        return new ExceptionMessage("No Entity Found by this id");
+    }
+    @ExceptionHandler(value={Exception.class})
+    @ResponseStatus(value=HttpStatus.INTERNAL_SERVER_ERROR)
+    public ExceptionMessage exceptionHandler(Exception e)
+    {
+        return new ExceptionMessage("Something Went Wrong");
+    }
+}
