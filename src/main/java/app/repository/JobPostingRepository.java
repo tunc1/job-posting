d7931309f80b5c8dca78aa3dacd9cc7f2132845d
user <user>
2021-07-06 22:28:39 +0300

    Update JobPostingRepository

--- src/main/java/app/repository/JobPostingRepository.java
+++ src/main/java/app/repository/JobPostingRepository.java
@@ -1,6 +1,12 @@
 package app.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import app.entity.JobPosting;
 
-public interface JobPostingRepository extends JpaRepository<JobPosting,Long>{}
+public interface JobPostingRepository extends JpaRepository<JobPosting,Long>
+{
+    List<JobPosting> findByCompanyId(long companyId);
+    List<JobPosting> findByTitleContaining(String query);
+}
