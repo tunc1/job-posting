9196c48db29ba13fdd13817956d92ef68b1056cb
user <user>
2021-07-06 22:17:21 +0300

    Update CompanyService Add belongsTo

--- src/main/java/app/service/CompanyService.java
+++ src/main/java/app/service/CompanyService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import javax.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import app.entity.Company;
+import app.entity.Member;
+import app.exception.UnauthorizedException;
 import app.repository.CompanyRepository;
 
 @Service
@@ -21,5 +23,12 @@ public class CompanyService
 	public List<Company> findAll()
 	{
 		return companyRepository.findAll();
+	}
+	public boolean belongsTo(Long id,Member member)
+	{
+		Company company=findById(id);
+		if(company.getManager().equals(member))
+			return true;
+		throw new UnauthorizedException();
 	}
 }
--- src/test/java/app/service/CompanyServiceTest.java
+++ src/test/java/app/service/CompanyServiceTest.java
@@ -14,6 +14,8 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import app.entity.Company;
+import app.entity.Member;
+import app.exception.UnauthorizedException;
 import app.repository.CompanyRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -56,5 +58,29 @@ public class CompanyServiceTest
             return Optional.empty();
         });
         Assertions.assertThrows(EntityNotFoundException.class,()->companyService.findById(1L));
+    }
+    @Test
+    void testBelongsTo_returnsTrue()
+    {
+        Member member=new Member();
+        member.setId(1L);
+        Company company=new Company();
+        CompanyService companyService2=Mockito.spy(companyService);
+        Mockito.doReturn(company).when(companyService2).findById(Mockito.anyLong());
+        company.setManager(member);
+        Assertions.assertTrue(companyService2.belongsTo(1L,member));
+    }
+    @Test
+    void testBelongsTo_throwsUnauthorizedException()
+    {
+        Member member=new Member();
+        member.setId(1L);
+        Member member2=new Member();
+        member.setId(2L);
+        Company company=new Company();
+        CompanyService companyService2=Mockito.spy(companyService);
+        Mockito.doReturn(company).when(companyService2).findById(Mockito.anyLong());
+        company.setManager(member2);
+        Assertions.assertThrows(UnauthorizedException.class,()->companyService2.belongsTo(1L,member));
     }
 }
