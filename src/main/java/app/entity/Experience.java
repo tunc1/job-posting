568330682f4267f5ec41c78624440fb28dd19ee7
user <user>
2021-07-13 20:12:53 +0300

    Update Experience Add title, summary

--- src/main/java/app/entity/Experience.java
+++ src/main/java/app/entity/Experience.java
@@ -18,6 +18,23 @@ public class Experience
     @JsonProperty(access=JsonProperty.Access.WRITE_ONLY)
     private Member member;
     private Date startedAt,leftAt;
+    private String title,summary;
+    public String getTitle()
+    {
+        return title;
+    }
+    public void setTitle(String title)
+    {
+        this.title=title;
+    }
+    public String getSummary()
+    {
+        return summary;
+    }
+    public void setSummary(String summary)
+    {
+        this.summary=summary;
+    }
     public Long getId()
     {
         return id;
