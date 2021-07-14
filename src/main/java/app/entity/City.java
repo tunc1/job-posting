1a78276d68c01c1e96f1bfbc365d4cbcddc37f2b
user <user>
2021-07-14 20:45:11 +0300

    Add City

---
+++ src/main/java/app/entity/City.java
@@ -0,0 +1,40 @@
+package app.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class City
+{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private Country country;
+    private String name;
+    public Long getId()
+    {
+        return id;
+    }
+    public void setId(Long id)
+    {
+        this.id=id;
+    }
+    public Country getCountry()
+    {
+        return country;
+    }
+    public void setCountry(Country country)
+    {
+        this.country=country;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+}
