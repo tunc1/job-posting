eb4780271a6ca6f432ee81c1d62cb4d825d2a111
user <user>
2021-08-07 21:14:51 +0300

    Update User Add id get set

--- src/main/java/app/entity/User.java
+++ src/main/java/app/entity/User.java
@@ -24,6 +24,14 @@ public class User implements UserDetails
     private String password;
     private boolean accountNonExpired,accountNonLocked,credentialsNonExpired,enabled;
     private String role;
+    public Long getId()
+    {
+        return id;
+    }
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
     public Collection<? extends GrantedAuthority> getAuthorities()
     {
         return List.of(new SimpleGrantedAuthority("ROLE_"+role));
