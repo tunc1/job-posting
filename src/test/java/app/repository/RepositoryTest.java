7afa53560c1874275442b2c41bf23f97f50aa328
user <email>
2021-08-14 12:03:23 +0300

    Add RepositoryTest

---
+++ src/test/java/app/repository/RepositoryTest.java
@@ -0,0 +1,11 @@
+package app.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class RepositoryTest
+{
+    @Test
+    void testContextLoads(){}
+}
