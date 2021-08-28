b04736e3152ee7c7800e2936f10da0bad4ce058d
user <email>
2021-08-28 19:10:44 +0300

    Add TokenResponse.java

--- src/main/java/app/controller/Authenticate.java
+++ src/main/java/app/controller/Authenticate.java
@@ -45,19 +45,3 @@ public class Authenticate
         }
     }
 }
-class TokenResponse
-{
-    private String token;
-    public TokenResponse(String token)
-    {
-        this.token=token;
-    }
-    public String getToken()
-    {
-        return token;
-    }
-    public void setToken(String token)
-    {
-        this.token=token;
-    }
-}
